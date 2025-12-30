0f44d1608d971dfe61b0aae3c8add9a1cf7ca8d2
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-12-30 13:16:16 -0600

    Add versioning and server initialization logic

--- pocketmine-api/pocketmine/VersionInfo.php
+++ pocketmine-api/pocketmine/VersionInfo.php
@@ -1,9 +1,29 @@
 <?php
 namespace pocketmine;
 use cn\nukkit\Nukkit;
+use pocketmine\Server;
+
 const _VERSION_INFO_INCLUDED = true;
 const BASE_VERSION = "3.23.1";
 const NAME = "PM-NK";
 const BUILD_CHANNEL = "stable";
 const IS_DEVELOPMENT_BUILD = false;
 const BUILD_NUMBER = 0;
+$version = BASE_VERSION;
+if(IS_DEVELOPMENT_BUILD){
+  $version .= "+dev";
+  if(BUILD_NUMBER > 0){
+    $version .= "." . BUILD_NUMBER;
+  }
+}
+define('pocketmine\VERSION', $version);
+$git_info = Nukkit::GIT_INFO;
+if($git_info != null){
+  define('pocketmine\GIT_COMMIT', $git_info->getProperty('git.commit.id.abbrev'));
+}else{
+  define('pocketmine\GIT_COMMIT', 'null');
+}
+define('pocketmine\DATA', Nukkit::DATA_PATH);
+define('pocketmine\PLUGIN_PATH', Nukkit::PLUGIN_PATH . DIRECTORY_SEPARATOR);
+define('pocketmine\START_TIME', Nukkit::START_TIME);
+new Server();
