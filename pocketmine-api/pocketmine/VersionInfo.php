416e918694b0ccbd5d8d311d8dcbde5cac2352f3
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2025-12-30 11:51:19 -0600

    Add VersionInfo.php with version constants

---
+++ pocketmine-api/pocketmine/VersionInfo.php
@@ -0,0 +1,9 @@
+<?php
+namespace pocketmine;
+use cn\nukkit\Nukkit;
+const _VERSION_INFO_INCLUDED = true;
+const BASE_VERSION = "3.23.1";
+const NAME = "PM-NK";
+const BUILD_CHANNEL = "stable";
+const IS_DEVELOPMENT_BUILD = false;
+const BUILD_NUMBER = 0;
