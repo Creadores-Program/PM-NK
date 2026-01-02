eed5db5d0141e7b7a4c1eda6c830ffa5de63255f
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-01-01 23:12:34 -0600

    Refactor VersionInfo to use JavaClass for Nukkit

--- pocketmine-api/src/pocketmine/VersionInfo.php
+++ pocketmine-api/src/pocketmine/VersionInfo.php
@@ -2,11 +2,12 @@
 declare(strict_types=1);
 
 namespace pocketmine;
-use cn\nukkit\Nukkit;
+use php\lang\JavaClass;
 use pocketmine\Server;
 use function define;
 use const DIRECTORY_SEPARATOR;
 
+$nukkit = new JavaClass("cn.nukkit.Nukkit");
 const _VERSION_INFO_INCLUDED = true;
 const BASE_VERSION = "3.23.1";
 const NAME = "PM-NK";
@@ -21,14 +22,14 @@ if(IS_DEVELOPMENT_BUILD){
   }
 }
 define('pocketmine\VERSION', $version);
-$git_info = Nukkit::GIT_INFO;
+$git_info = $nukkit->getDeclaredField("GIT_INFO")->get(null);
 if($git_info != null){
   define('pocketmine\GIT_COMMIT', $git_info->getProperty('git.commit.id.abbrev'));
 }else{
   define('pocketmine\GIT_COMMIT', 'null');
 }
-define('pocketmine\DATA', Nukkit::DATA_PATH);
-define('pocketmine\PLUGIN_PATH', Nukkit::PLUGIN_PATH . DIRECTORY_SEPARATOR);
-define('pocketmine\START_TIME', Nukkit::START_TIME);
-define('pocketmine\RESOURCE_PATH', Nukkit::DATA_PATH . "resource_packs" . DIRECTORY_SEPARATOR);
+define('pocketmine\DATA', $nukkit->getDeclaredField("DATA_PATH")->get(null));
+define('pocketmine\PLUGIN_PATH', $nukkit->getDeclaredField("PLUGIN_PATH")->get(null) . DIRECTORY_SEPARATOR);
+define('pocketmine\START_TIME', $nukkit->getDeclaredField("START_TIME")->get(null));
+define('pocketmine\RESOURCE_PATH', $nukkit->getDeclaredField("DATA_PATH")->get(null) . "resource_packs" . DIRECTORY_SEPARATOR);
 new Server();
