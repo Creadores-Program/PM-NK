1a550ad4b3f995f856d5e1cbedd2d451bb756cdd
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-01-01 22:29:30 -0600

    Implement onEnable method for plugin loading
    Added onEnable method to register PMPluginLoader and load plugins.

--- pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/Main.java
+++ pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/Main.java
@@ -1,5 +1,9 @@
 package org.CreadoresProgram.PMNK;
 import cn.nukkit.plugin.PluginBase;
+import cn.nukkit.plugin.PluginLoadOrder;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File;
 public class Main extends PluginBase{
     private static Main instance;
     public static Main getInstance(){
@@ -9,4 +13,13 @@ public class Main extends PluginBase{
     public void onLoad(){
         instance = this;
     }
+    @Override
+    public void onEnable(){
+        this.getServer().getPluginManager().registerInterface(PMPluginLoader.class);
+        List<String> loaders = new ArrayList<>();
+        loaders.add(PMPluginLoader.class.getName());
+        this.getServer().getPluginManager().loadPlugins(new File(this.getServer().getPluginPath()), loaders, false);
+        this.getServer().enablePlugins(PluginLoadOrder.STARTUP);
+    }
 }
+
