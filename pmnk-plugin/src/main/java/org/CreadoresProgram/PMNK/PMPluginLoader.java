bb4bb0b7cf2f23a37cb34aa917590d44cbe8e3ad
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-01-06 19:14:59 -0600

    Add the Plugin Description feature

--- pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/PMPluginLoader.java
+++ pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/PMPluginLoader.java
@@ -1,14 +1,19 @@
 package org.CreadoresProgram.PMNK;
+import cn.nukkit.Nukkit;
 import cn.nukkit.Server;
 import cn.nukkit.event.plugin.PluginDisableEvent;
 import cn.nukkit.event.plugin.PluginEnableEvent;
+import cn.nukkit.utils.PluginException;
 import cn.nukkit.plugin.*;
+import org.yaml.snakeyaml.Yaml;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.*;
 import java.io.InputStream;
 import java.util.*;
 import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
 import java.util.regex.Pattern;
 import java.nio.charset.StandardCharsets;
 import php.runtime.env.CompileScope;
@@ -27,7 +32,9 @@ public class PMPluginLoader implements PluginLoader{
   private Server server;
   private CompileScope scope;
   private Environment env;
+  private Map<String, Map<String, byte[]>> pluginsPM = new HashMap<>();
   private static final Pattern[] FILTERS = new Pattern[]{Pattern.compile("^.+\\.phar$")};
+  private static final String[] apiVersion = "3.23.1".split("\\.");
   public PMPluginLoader(Server server){
     this.server = server;
     this.plugin = Main.getInstance();
@@ -62,6 +69,7 @@ public class PMPluginLoader implements PluginLoader{
   }
   @Override
   public Plugin loadPlugin(File file) throws Exception {
+    this.registerPathPlugin(file);
     //no implemented
     return null;
   }
@@ -71,8 +79,100 @@ public class PMPluginLoader implements PluginLoader{
   }
   @Override
   public PluginDescription getPluginDescription(File file){
-    //no implemented
-    return null;
+    this.registerPathPlugin(file);
+    Map<String, byte[]> pluginDir = this.pluginsPM.get(file.getAbsolutePath());
+    if(!pluginDir.containsKey("plugin.yml")){
+      return null;
+    }
+    Yaml yaml = new Yaml();
+    Map<String, Object> pluginYmlPm = yaml.load(new String(pluginDir.get("plugin.yml"), StandardCharsets.UTF_8));
+    if(!this.checkCompatibility(((String) pluginYmlPm.get("name")), pluginYmlPm.get("api"), true)){
+      return null;
+    }
+    Map<String, Object> pluginYmlNk = new HashMap<>();
+    pluginYmlNk.put("api", Nukkit.API_VERSION);
+    pluginYmlNk.put("name", pluginYmlPm.get("name"));
+    pluginYmlNk.put("version", pluginYmlPm.get("version"));
+    String main = String.valueOf(pluginYmlPm.get("main"));
+    if(main.startsWith("pocketmine\\")){
+      this.server.getLogger().error("Error: ", new PluginException("Invalid Plugin main, cannot start within the PocketMine namespace"));
+      return null;
+    }
+    pluginYmlNk.put("main", main);
+    if(pluginYmlPm.constainsKey("commands")){
+      pluginYmlNk.put("commands", pluginYmlPm.get("commands"));
+    }
+    if(pluginYmlPm.constainsKey("depend")){
+      pluginYmlNk.put("depend", pluginYmlPm.get("depend"));
+    }
+    if(pluginYmlPm.constainsKey("softdepend")){
+      pluginYmlNk.put("softdepend", pluginYmlPm.get("softdepend"));
+    }
+    if(pluginYmlPm.constainsKey("loadbefore")){
+      pluginYmlNk.put("loadbefore", pluginYmlPm.get("loadbefore"));
+    }
+    if(pluginYmlPm.constainsKey("website")){
+      pluginYmlNk.put("website", pluginYmlPm.get("website"));
+    }
+    if(pluginYmlPm.constainsKey("description")){
+      pluginYmlNk.put("description", pluginYmlPm.get("description"));
+    }
+    if(pluginYmlPm.constainsKey("prefix")){
+      pluginYmlNk.put("prefix", pluginYmlPm.get("prefix"));
+    }
+    if(pluginYmlPm.constainsKey("load")){
+      pluginYmlNk.put("load", pluginYmlPm.get("load"));
+    }
+    if(pluginYmlPm.constainsKey("author")){
+      pluginYmlNk.put("author", pluginYmlPm.get("author"));
+    }
+    if(pluginYmlPm.constainsKey("authors")){
+      pluginYmlNk.put("authors", pluginYmlPm.get("authors"));
+    }
+    if(pluginYmlPm.constainsKey("permissions")){
+      pluginYmlNk.put("permissions", pluginYmlPm.get("permissions"));
+    }
+    return new PluginDescription(pluginYmlNk);
+  }
+  @SuppressWarnings("unchecked")
+  private boolean checkCompatibility(String name, Object api, boolean posibleArray){
+    if(api instanceof List && posibleArray){
+      return checkCompatibility(name, (List<Object>) api);
+    }
+    String version = String.valueOf(api);
+    try {
+      //Check the format: majorVersion.minorVersion.patch
+      if (!Pattern.matches("^[0-9]+\\.[0-9]+\\.[0-9]+$", version)) {
+        throw new IllegalArgumentException();
+      }
+    } catch (NullPointerException | IllegalArgumentException e) {
+      this.server.getLogger().error(this.server.getLanguage().translateString("nukkit.plugin.loadError", new String[]{name, "Wrong API format"}));
+      return false;
+    }
+    String[] versionArray = version.split("\\.");
+    if (!Objects.equals(Integer.valueOf(versionArray[0]), Integer.valueOf(apiVersion[0]))) {
+      return false;
+    }
+    if (Integer.parseInt(versionArray[1]) > Integer.parseInt(apiVersion[1])) {
+      return false;
+    }
+    return true;
+  }
+  private boolean checkCompatibility(String name, List<Object> apis){
+    boolean compatible = false;
+    for(Object apiC : apis){
+      if(this.checkCompatibility(name, apiC, false)){
+        compatible = true;
+      }
+    }
+    return compatible;
+  }
+  private void registerPathPlugin(File file){
+    String absolutePathPlugin = file.getAbsolutePath();
+    if(this.pluginsPM.containsKey(absolutePathPlugin)){
+      return;
+    }
+    this.pluginsPM.put(absolutePathPlugin, PharManager.readPhar(file));
   }
   @Override
   public Pattern[] getPluginFilters(){
@@ -93,6 +193,7 @@ public class PMPluginLoader implements PluginLoader{
         this.server.getServiceManager().cancel(plugin);
         this.server.getPluginManager().callEvent(new PluginDisableEvent(plugin));
         ((PluginBase) plugin).setEnabled(false);
+        this.pluginsPM.remove(((PluginBase) plugin.getFile()).getAbsolutePath());
     }
   }
   private void eval(byte[] code, String modulePath){
