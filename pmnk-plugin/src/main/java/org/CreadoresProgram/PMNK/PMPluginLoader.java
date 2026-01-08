c1071a2500626c187ab9ecdd547b646e1a39502c
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-01-07 23:14:33 -0600

    Update PMPluginLoader.java

--- pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/PMPluginLoader.java
+++ pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/PMPluginLoader.java
@@ -61,7 +61,9 @@ public class PMPluginLoader implements PluginLoader{
       this.eval(entry.getValue(), fileName);
     }
     this.eval(pmDir.get("pocketmine/VersionInfo.php"), "pocketmine/VersionInfo.php");
-    this.eval("<?php\n\\pocketmine\\Server::getInstance()->getLogger()->info('Hola mundoo');\n", "Unknown");
+    this.eval("<?php\nuse \\cn\\nukkit\\Server;\nServer::getInstance()->getLogger()->info('Hola Mundo!');", "Unknown1.php");
+    this.eval("<?php\nuse \\php\\lang\\JavaClass;\n$serverNk = new JavaClass('cn.nukkit.Server');\n$serverNk->getInstance()->getLogger()->info('Hola mundo!');", "Unknown2.php");
+    this.eval("<?php\n\\pocketmine\\Server::getInstance()->getLogger()->info('Hola mundoo');", "Unknown.php");
   }
   @Override
   public Plugin loadPlugin(String filename) throws Exception {
