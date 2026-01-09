6d986e890af2a78c1fb5baf366b36a7ed29331f8
Trollhunters501PC <cpcreador@gmail.com>
2026-01-08 22:01:59 -0600

    Test3

--- pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/NukkitExtension.java
+++ pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/NukkitExtension.java
@@ -1,6 +1,7 @@
 package org.CreadoresProgram.PMNK;
 import php.runtime.ext.support.Extension;
 import php.runtime.env.CompileScope;
+import php.runtime.reflection.ClassEntity;
 import cn.nukkit.Server;
 import cn.nukkit.Nukkit;
 public class NukkitExtension extends Extension{
@@ -11,5 +12,20 @@ public class NukkitExtension extends Extension{
   @Override
   public void onRegister(CompileScope scope) {
     registerClass(scope, Server.class, Nukkit.class);
+  }
+  protected void registerClass(CompileScope scope, Class<?>... classes) {
+    for (Class<?> clazz : classes) {
+      String className = clazz.getName().replace('.', '\\');
+      ClassEntity entity = new ClassEntity(scope);
+      entity.setName(className);
+      entity.setNativeClazz(clazz);
+      ClassEntity javaObjectEntity = scope.fetchUserClass(JavaObject.class);
+      if (javaObjectEntity != null) {
+        entity.setParent(javaObjectEntity);
+      } else {
+          entity.setParent(scope.fetchUserClass("php\\lang\\JavaObject"));
+      }
+      scope.addUserClass(entity);
+    }
   }
 }
