77da5fcd66f81e0c33c3ffb3cc02708941d9a199
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-01-08 23:23:03 -0600

    Create NukkitExtension.java

---
+++ pmnk-plugin/src/main/java/org/CreadoresProgram/PMNK/NukkitExtension.java
@@ -0,0 +1,15 @@
+package org.CreadoresProgram.PMNK;
+import php.runtime.ext.support.Extension;
+import php.runtime.env.CompileScope;
+import cn.nukkit.Server;
+import cn.nukkit.Nukkit;
+public class NukkitExtension extends Extension{
+  @Override
+  public Status getStatus() {
+    return Status.STABLE;
+  }
+  @Override
+  public void onRegister(CompileScope scope) {
+    registerClass(scope, Server.class, Nukkit.class);
+  }
+}
