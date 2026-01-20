bca46f4ac6265d3d48e6fb53938edad6b015eef4
Trollhunters501 <91704943+Trollhunters501@users.noreply.github.com>
2026-01-20 08:14:33 -0600

    Add PositionPm class extending Vector3Pm

---
+++ pmnk-plugin/src/main/java/pocketmine/level/PositionPm.java
@@ -0,0 +1,28 @@
+package pocketmine.level;
+import java.util.Arrays;
+import pocketmine.math.Vector3Pm;
+import pocketmine.ServerPm;
+import php.runtime.env.Environment;
+import php.runtime.reflection.ClassEntity;
+import php.runtime.Memory;
+import php.runtime.memory.StringMemory;
+import php.runtime.lang.spl.exception.BadMethodCallException;
+import static php.runtime.annotation.Reflection.*;
+@Name("Position")
+@Namespace("pocketmine\\level")
+public class PositionPm extends Vector3Pm{
+    private Position instanceNK;
+    public PositionPm(Environment env, Position instanceNK){
+        super(env);
+        this.instanceNK = instanceNK;
+    }
+    public PositionPm(Environment env, ClassEntity clazz){
+        super(env, clazz);
+    }
+    @Signature
+    public void __construct(Environment env){
+        if(this.instanceNK == null){
+            this.instanceNK = new Position();
+        }
+    }
+}
