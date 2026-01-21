24ca09badb311a37b2b6bbcf58800fe381f147f6
Trollhunters501PC <cpcreador@gmail.com>
2026-01-20 21:00:05 -0600

    Add more methods for pocketmine\level\Position

--- pmnk-plugin/src/main/java/pocketmine/level/PositionPm.java
+++ pmnk-plugin/src/main/java/pocketmine/level/PositionPm.java
@@ -13,6 +13,7 @@ import static php.runtime.annotation.Reflection.*;
 @Namespace("pocketmine\\level")
 public class PositionPm extends Vector3Pm{
     private Position instanceNK;
+    private Memory level;
     public PositionPm(Environment env, Position instanceNK){
         super(env, instanceNK);
         this.instanceNK = instanceNK;
@@ -25,5 +26,81 @@ public class PositionPm extends Vector3Pm{
         if(this.instanceNK == null){
             this.instanceNK = new Position();
         }
+    }
+    @Signature
+    public void __construct(Environment env, int x){
+        this.instanceNK = new Position((double) x);
+    }
+    @Signature
+    public void __construct(Environment env, float x){
+        this.instanceNK = new Position((double) x);
+    }
+    @Signature
+    public void __construct(Environment env, int x, int y){
+        this.instanceNK = new Position(((double) x), ((double) y));
+    }
+    @Signature
+    public void __construct(Environment env, float x, float y){
+        this.instanceNK = new Position(((double) x), ((double) y));
+    }
+    @Signature
+    public void __construct(Environment env, int x, int y, int z){
+        this.instanceNK = new Position(((double) x), ((double) y), ((double) z));
+    }
+    @Signature
+    public void __construct(Environment env, float x, float y, float z){
+        this.instanceNK = new Position(((double) x), ((double) y), ((double) z));
+    }
+    /*
+    @Signature
+    public void __construct(Environment env, int x, int y, int z, LevelPm level){
+        this.instanceNK = new Position(((double) x), ((double) y), ((double) z), level.getNK());
+    }
+    @Signature
+    public void __construct(Environment env, float x, float y, float z, LevelPm level){
+        this.instanceNK = new Position(((double) x), ((double) y), ((double) z), level.getNK());
+    }
+    */
+    @Signature
+    public static PositionPm fromObject(Environment env, Vector3Pm pos){
+        PositionPm posn = new PositionPm(env, Position.fromObject(pos.getNK()));
+        try{
+            env.invokeMethod(posn, "__construct");
+        }catch(Throwable e){
+            e.printStackTrace();
+        }
+        return posn;
+    }
+    /*
+    @Signature
+    public static PositionPm fromObject(Environment env, Vector3Pm pos, LevelPm level){
+        PositionPm posn = new PositionPm(env, Position.fromObject(pos.getNK(), level.getNK()));
+        try{
+            env.invokeMethod(posn, "__construct");
+        }catch(Throwable e){
+            e.printStackTrace();
+        }
+        return posn;
+    }
+    */
+    /*
+    @Signature
+    public void __set(String name, LevelPm val){
+        if(name.equals("level")){
+            this.instanceNK.level = val.getNK();
+            this.level = (Memory) new php.runtime.memory.ObjectMemory(val);
+        }
+    }
+    @Signature
+    public Memory __get(String name){
+        if(name.equals("level")){
+            return this.level;
+        }
+        return super.__get(name);
+    }
+    */
+    @Ignore
+    public PositionPm getNK(){
+        return this.instanceNK;
     }
 }
