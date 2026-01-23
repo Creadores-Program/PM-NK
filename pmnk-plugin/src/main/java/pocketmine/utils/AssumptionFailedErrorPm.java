23d9f8b515ed05fead14b766905901f6a59c83b4
Trollhunters501PC <cpcreador@gmail.com>
2026-01-22 20:54:16 -0600

    Fix bug

--- pmnk-plugin/src/main/java/pocketmine/utils/AssumptionFailedErrorPm.java
+++ pmnk-plugin/src/main/java/pocketmine/utils/AssumptionFailedErrorPm.java
@@ -1,5 +1,6 @@
 package pocketmine.utils;
 import php.runtime.lang.exception.BaseError;
+import php.runtime.env.Environment;
 import static php.runtime.annotation.Reflection.*;
 @Name("AssumptionFailedError")
 @Namespace("pocketmine\\utils")
